2079d349f5adee62014ba8d21b73e1aec1957fe3
Harshthakar12 <hthakar812@gmail.com>
2025-09-09 09:32:14 +0530

    Add files via upload

---
+++ if-else.cpp
@@ -0,0 +1,14 @@
+//conditional statements
+//check if the number is positive or negative
+ #include <iostream>
+using namespace std;
+ int main(){
+ int num;
+ cout<<"Enter A Num :";
+ cin>>num;
+ if(num>=0){
+cout<<"Number Is Positive"<<endl;
+}else{
+cout<<"Number Is Negative "<<endl;
+}
+}   
