7f5ec36555580a67fdf3f551ceea415c71736bb0
Harshthakar12 <hthakar812@gmail.com>
2025-09-09 09:04:19 +0530

    arithmeticoperator.cpp

--- arithmeticoperator.cpp
+++ arithmeticoperator.cpp
@@ -1,7 +1,8 @@
  #include <iostream>
 using namespace std;
 int main(){
-int a  int b;
+int a;
+int b;
 cin>>a;
 cin>>b;
 cout<<(a+b)<<endl;
@@ -9,3 +10,4 @@ cout<<(a-b)<<endl;
 cout<<(a*b)<<endl;
 cout<<(a/b)<<endl;
 cout<<(a%b)<<endl;
+}
