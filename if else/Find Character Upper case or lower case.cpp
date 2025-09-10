019f3b86d81f4b83e1e881ced2977448e12dbcfb
Harshthakar12 <hthakar812@gmail.com>
2025-09-10 07:48:56 +0530

    Add files via upload

---
+++ if else/Find Character Upper case or lower case.cpp
@@ -0,0 +1,15 @@
+Find Character Upper case or lower case
+
+#include <iostream>
+using namespace std;
+int main (){
+char ch;
+cout<<"Enter A character :";
+cin>>ch;
+if(ch>='a' && ch<='z'){
+cout<<"Lower Case";
+}
+else{
+ cout<<"Upper Case";
+}
+}
